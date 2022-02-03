111d15a2ddadf4232c32d4773a16bb731d4ee979
TanyaZholnerkevich <79047022+TanyaZholnerkevich@users.noreply.github.com>
2022-02-03 16:33:52 +0300

    Add Bomberman

---
+++ Assets/Scripts/BombExplosion.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BombExplosion : MonoBehaviour
+{
+    [SerializeField] private float radius = 0.5f;
+    [SerializeField] private LayerMask bombMask;
+    [SerializeField] private GameObject explosion;
+
+    private void Start()
+    {
+        explosion.SetActive(false);
+    }
+
+    private void Update()
+    {
+        StartCoroutine(Explosion());
+    }
+
+    private IEnumerator Explosion()
+    {
+        yield return new WaitForSeconds(3f);
+        explosion.SetActive(true);
+        var colliders = Physics2D.OverlapCircleAll(transform.position, radius, bombMask);
+        foreach (var cldr in colliders)
+        {
+            Destroy(cldr.gameObject);
+            /*if (cldr.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(0);
+                Debug.Log("YOU LOOSER");
+            }*/
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    //[SerializeField] private LayerMask raycastMask;
+    //[SerializeField] private LayerMask explosionMask;
+    [SerializeField] private GameObject bomb;
+    
+    private bool isMovement;
+
+    private void Update()
+    {
+        if (isMovement)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MovePlayerTo(Vector2.left);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MovePlayerTo(Vector2.right);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MovePlayerTo(Vector2.up);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MovePlayerTo(Vector2.down);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bomb, transform.position, Quaternion.identity);
+        }
+
+        StartCoroutine(Destroy());
+    }
+
+    private void MovePlayerTo(Vector2 dir)
+    {
+        if (Raycast(dir))
+        {
+            return;
+        }
+
+        isMovement = true;
+
+        var pos = (Vector2)transform.position + dir;
+        transform.DOMove(pos, 0.2f).OnComplete(() =>
+        {
+            isMovement = false;
+        }).SetEase(Ease.Linear);
+    }
+
+    private bool Raycast(Vector2 dir)
+    {
+        var hit = Physics2D.Raycast(transform.position, dir, 1f/*, raycastMask*/);
+        return hit.collider != null;
+    }
+
+    private IEnumerator Destroy()
+    {
+        yield return new WaitForSecondsRealtime(3f);
+        var distance = Vector2.Distance(transform.position, bomb.transform.position);
+        if (distance < 0.5f)
+        {
+            SceneManager.LoadScene(0);
+            Debug.Log("YOU LOOSER");
+        }
+    }
+}
